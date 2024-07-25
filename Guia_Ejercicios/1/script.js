fcc57bdc369c7ed07dc1fff5b0b9ffbc6ee8496d
reyesmario213 <marioreyes@liceovvh.cl>
2024-07-25 12:16:09 -0400

    ok

---
+++ Guia_Ejercicios/1/script.js
@@ -0,0 +1,8 @@
+let nombre = prompt("Ingrese su nombre por favor: ");
+let apellido = prompt("Ingrese su apellido por favor: ");
+let edad = prompt("Ingrese su edad Por favor: ");
+
+console.log("\nLos datos que ingresaste son:");
+console.log(`Nombre: ${nombre}`);
+console.log(`Apellido: ${apellido}`);
+console.log(`Edad: ${edad}`);
