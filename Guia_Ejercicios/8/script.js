543cbf4f638cb4b737399ee76d8a69dca5f0d46b
reyesmario213 <marioreyes@liceovvh.cl>
2024-08-05 11:14:21 -0400

    ok

---
+++ Guia_Ejercicios/8/script.js
@@ -0,0 +1,11 @@
+let suma = 0;
+
+function sumarNumeros() {
+    for (let i = 1; i <= 5; i++) {
+        let numero = parseInt(prompt(`Ingrese el número ${i}:`));
+        suma += numero;
+    }
+}
+
+sumarNumeros();
+console.log(`La suma de los 5 números es: ${suma}`);
